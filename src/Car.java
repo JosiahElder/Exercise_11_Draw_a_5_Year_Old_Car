ee3866166e0154e549f1d5453289aa82243bf910
JosiahElder <@JosiahElder>
2023-03-31 19:47:35 -0700

    Next Commit

--- src/Car.java
+++ src/Car.java
@@ -3,12 +3,21 @@ import java.awt.Color;
 
 public class Car {
     private Color bodyColor;
+    private int height;
+    private int width;
+    private int xPosition;
+    private int yPosition;
 
-    public Car(Color bodyColor) {
+    public Car(Color bodyColor, int height, int width, int xPosition, int yPosition) {
         this.bodyColor = bodyColor;
+        this.height = height;
+        this.width = width;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
     }
 
     public void drawVehicle(Graphics g) {
         g.setColor(bodyColor);
+        g.fillRect(xPosition, yPosition, width, height);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,20 @@ import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Color;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         JFrame frame = new JFrame("Car");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
 
-        Car car = new Car(Color.RED);
+        Car car = new Car(Color.RED, 50, 100, 50, 150);
 
         JPanel panel = new JPanel() {
             @Override
-            public void paintComponent(Graphics g) {
+            public void paintComponent(Graphics g)
+            {
                 super.paintComponent(g);
                 car.drawVehicle(g);
             }
